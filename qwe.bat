c708e051ec596a58bf0e6dda0210d800957adf0c
aghkbb <50142544+aghkbb@users.noreply.github.com>
2021-12-21 11:23:02 +0800

    Create qwe.bat

---
+++ qwe.bat
@@ -0,0 +1,10 @@
+:: This line just in case someone accidentally double-clicks this file
+start "" "%ProgramFiles(x86)%\Notepad++\notepad++.exe" "%CD%\qwe.bat"
+goto :eof
+
+:::::::::::::::::::::::
+:: Maven Plugin Goals::
+:::::::::::::::::::::::
+
+mvn clean
+:: clean
