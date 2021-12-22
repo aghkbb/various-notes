63f19f637f741b151b7fd035db323bf42e6a6616
aghkbb <50142544+aghkbb@users.noreply.github.com>
2021-12-22 15:37:07 +0800

    Update qwe.bat

--- qwe.bat
+++ qwe.bat
@@ -8,3 +8,31 @@ goto :eof
 
 mvn clean
 :: clean
+
+:::::::::::::::::::::
+:: Docker Commands ::
+:::::::::::::::::::::
+
+:: Build application
+:: Assign name 'getting-started' to container
+:: Use Dockerfile in current directory (.)
+docker build -t getting-started .
+
+:: Run container 'getting-started'
+:: map port 3000 to port 3000
+docker run -dp 3000:3000 getting-started
+
+:: List running containers
+docker ps
+
+:: Stop container
+docker stop <CONTAINER ID>
+
+:: Remove container
+docker rm <CONTAINER ID>
+
+:: Push to dockerhub
+docker push regulatedrot/getting-started
+
+:: Rename image
+docker tag getting-started regulatedrot/getting-started
